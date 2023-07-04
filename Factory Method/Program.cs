18ba75928b1a0eb1a78d1efe2cabe1d4020b2568
Hoa Nguyen <hoann.2016@gmail.com>
2023-07-04 16:04:05 +0700

    Initialize

---
+++ Factory Method/Program.cs
@@ -0,0 +1,23 @@
+﻿// Note: constructors call Factory Method
+
+using Factory_Method;
+
+Document[] documents = new Document[2];
+
+documents[0] = new Resume();
+documents[1] = new Report();
+
+// Display document pages
+
+foreach (Document document in documents)
+{
+    Console.WriteLine("\n" + document.GetType().Name + "--");
+    foreach (Page page in document.Pages)
+    {
+        Console.WriteLine(" " + page.GetType().Name);
+    }
+}
+
+// Wait for user
+
+Console.ReadKey();
