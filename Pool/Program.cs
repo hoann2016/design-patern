8fe8593eecb93889dfda79345f896744d80532b8
Hoa Nguyen <hoann.2016@gmail.com>
2023-07-10 11:40:10 +0700

    Add pull

---
+++ Pool/Program.cs
@@ -0,0 +1,14 @@
+﻿using Pool;
+
+Factory fa = new Factory();
+Student myStu = fa.GetStudent();
+Console.WriteLine("First object");
+Student myStu1 = fa.GetStudent();
+Console.WriteLine("Second object");
+Student myStu2 = fa.GetStudent();
+Console.WriteLine("Third object");
+Student myStu3 = fa.GetStudent();
+Console.WriteLine("Four object");
+Student myStu4 = fa.GetStudent();
+Console.WriteLine("fith object");
+Console.Read();
