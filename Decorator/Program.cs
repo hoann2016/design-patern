1c8fc33e480a1c9438d1fd1047a91343147711f1
Hoa Nguyen <hoann.2016@gmail.com>
2023-07-10 17:44:25 +0700

    add decorator

---
+++ Decorator/Program.cs
@@ -0,0 +1,15 @@
+﻿using Decorator;
+
+ICar objCar = new EconomyCar();
+
+//Wrp EconomyCar instancw with BasicAccessories.   
+CarAccessoriesDecorator objAccessoriesDecorator = new BasicAccessories(objCar);
+
+//Wrap EconomyCar instance with AdvancedAccessories instance.   
+objAccessoriesDecorator = new AdvancedAccessories(objAccessoriesDecorator);
+
+Console.Write("Car Detials: " + objAccessoriesDecorator.GetDescription());
+Console.WriteLine("\n\n");
+Console.Write("Total Price: " + objAccessoriesDecorator.GetCost());
+
+Console.Read();
---
+++ Decorator/models.cs
@@ -0,0 +1,151 @@
+﻿namespace Decorator;
+
+public interface ICar
+{
+    string GetDescription();
+    double GetCost();
+}
+
+public class EconomyCar : ICar
+{
+    public string GetDescription()
+    {
+        return "Economy Car";
+    }
+
+    public double GetCost()
+    {
+        return 450000.0;
+    }
+}
+
+public class DeluxCar : ICar
+{
+    public string GetDescription()
+    {
+        return "Delux Car";
+    }
+
+    public double GetCost()
+    {
+        return 750000.0;
+    }
+}
+
+public class LuxuryCar : ICar
+{
+    public string GetDescription()
+    {
+        return "Luxury Car";
+    }
+
+    public double GetCost()
+    {
+        return 1000000.0;
+    }
+}
+
+public abstract class CarAccessoriesDecorator : ICar
+{
+    private ICar _car;
+
+    public CarAccessoriesDecorator(ICar aCar)
+    {
+        _car = aCar;
+    }
+
+    public virtual string GetDescription()
+    {
+        return _car.GetDescription();
+    }
+
+    public virtual double GetCost()
+    {
+        return _car.GetCost();
+    }
+}
+
+public class BasicAccessories : CarAccessoriesDecorator
+{
+    public BasicAccessories(ICar aCar)
+        : base(aCar)
+    {
+    }
+
+    public override string GetDescription()
+    {
+        return base.GetDescription() + ",Basic Accessories Package";
+    }
+
+    public override double GetCost()
+    {
+        return base.GetCost() + 2000.0;
+    }
+}
+
+public class AdvancedAccessories : CarAccessoriesDecorator
+{
+    public AdvancedAccessories(ICar aCar)
+        : base(aCar)
+    {
+    }
+
+    public override string GetDescription()
+    {
+        return base.GetDescription() + ",Advanced Accessories Package";
+    }
+
+    public override double GetCost()
+    {
+        return base.GetCost() + 10000.0;
+    }
+}
+
+public class SportsAccessories : CarAccessoriesDecorator
+{
+    public SportsAccessories(ICar aCar)
+        : base(aCar)
+    {
+    }
+
+    public override string GetDescription()
+    {
+        return base.GetDescription() + ",Sports Accessories Package";
+    }
+
+    public override double GetCost()
+    {
+        return base.GetCost() + 15000.0;
+    }
+}
+
+public class SuperLuxury : ICar
+{
+    public string GetDescription()
+    {
+        return "Super Luxury Car";
+    }
+
+    public double GetCost()
+    {
+        return 1500000;
+    }
+}
+
+public class ExtremeSportAccessories : CarAccessoriesDecorator
+{
+    public ExtremeSportAccessories(ICar aCar)
+        : base(aCar)
+    {
+    }
+
+    public override string GetDescription()
+    {
+        return base.GetDescription() + ",Extreme Sport Accessories";
+    }
+
+    public override double GetCost()
+    {
+        return base.GetCost() + 25000;
+    }
+}
