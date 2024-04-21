40e7d2caa757db6454bbc3da0d0d6886b145a4d4
Hoa Nguyen <hoann.2016@gmail.com>
2024-04-21 13:01:33 +0700

    Add explanation for Factory Method

---
+++ Factory Method/Readme.md
@@ -0,0 +1,10 @@
+﻿# Code explanation
+Page là class trống
+Các class: SkillsPage,EducationPage,ExperiencePage,IntroductionPage,ResultsPage,ConclusionPage,
+SummaryPage,BibliographyPage
+Document là một abstract class, có abstract method CreatePages (implementation not yet)
+Report and Resume là 2 method kế thừa từ Document, and override CreatePages:
+- Report có page bao gồm: IntroductionPage,ResultsPage,ConclusionPage,SummaryPage,BibliographyPage
+- Resume có page bao gồm: SkillsPage,EducationPage,ExperiencePage
+Hiển nhiên khi thực thi một Array kiểu Document -> chi tiết thưc thi của method CreatePages có thể khác nhau dựa vào Document được
+implement bởi Ressume or Report
--- Factory Method/models.cs
+++ Factory Method/models.cs
@@ -3,20 +3,15 @@
     abstract class Page
     {
     }
-
     class SkillsPage : Page
     {
     }
-
     class EducationPage : Page
     {
     }
-
     class ExperiencePage : Page
     {
     }
-
-
     class IntroductionPage : Page
     {
     }
@@ -24,43 +19,31 @@
     class ResultsPage : Page
     {
     }
-
     class ConclusionPage : Page
     {
     }
-
-
     class SummaryPage : Page
     {
     }
 
-
     class BibliographyPage : Page
     {
     }
-
     abstract class Document
     {
         private List<Page> _pages = new List<Page>();
-
         // Constructor calls abstract Factory method
-
         public Document()
         {
             this.CreatePages();
         }
-
         public List<Page> Pages
         {
             get { return _pages; }
         }
-
         // Factory Method
-
         public abstract void CreatePages();
     }
-
-
     class Resume : Document
     {
         // Factory Method implementation
@@ -72,11 +55,9 @@
             Pages.Add(new ExperiencePage());
         }
     }
-
     class Report : Document
     {
         // Factory Method implementation
-
         public override void CreatePages()
         {
             Pages.Add(new IntroductionPage());
